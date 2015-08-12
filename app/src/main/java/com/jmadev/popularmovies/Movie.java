235769de87aec8bb81a6fda18b4a80f23052cb44
Jonathan <jae.antonio.29@gmail.com>
2015-08-11 20:57:50 -0700

    Use Parcelable instead of Serializable

--- app/src/main/java/com/jmadev/popularmovies/Movie.java
+++ app/src/main/java/com/jmadev/popularmovies/Movie.java
@@ -1,11 +1,10 @@
 package com.jmadev.popularmovies;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-/**
- * Created by Jae on 8/1/2015.
- */
-public class Movie implements Serializable {
+
+public class Movie implements Parcelable {
     private String backdropPath;
     private String originalTitle;
     private String overview;
@@ -13,6 +12,34 @@ public class Movie implements Serializable {
     private String posterPath;
     private String title;
     private double voteAverage;
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    public Movie (Parcel in) {
+        String[] sdata = new String[6];
+        double[] ddata = new double[1];
+
+        in.readStringArray(sdata);
+        in.readDoubleArray(ddata);
+
+        this.backdropPath = sdata[0];
+        this.originalTitle = sdata[1];
+        this.overview = sdata[2];
+        this.releaseDate = sdata[3];
+        this.posterPath = sdata[4];
+        this.title = sdata[5];
+        this.voteAverage = ddata[0];
+    }
 
     public Movie( String backdropPath, String originalTitle, String overview, String releaseDate, String posterPath, String title, double voteAverage) {
         this.backdropPath = backdropPath;
@@ -54,5 +81,26 @@ public class Movie implements Serializable {
     public String toString() {
         return "Movie: \n" + backdropPath + "\n" + originalTitle + "\n" + overview + "\n" + releaseDate + "\n" + posterPath +
                 "\n" + title + "\n" + voteAverage;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[] {
+            this.backdropPath,
+            this.originalTitle,
+            this.overview,
+            this.releaseDate,
+            this.posterPath,
+            this.title
+        });
+
+        dest.writeDoubleArray(new double[] {
+                this.voteAverage
+        });
     }
 }
